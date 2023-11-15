25f6804370d43cef55335cc3c6f539fc535f5bae
wizard503 <josemdg12@gmail.com>
2023-11-14 21:59:45 -0600

    Fixing error

--- src/hooks/useDiploma.js
+++ src/hooks/useDiploma.js
@@ -4,13 +4,12 @@ export const getDiploma = async (dui) => {
   try {
     const res = await fetch(ENDPOINT + dui);
 
-    // Muestra los encabezados de respuesta en la consola
     console.log('Headers:', res.headers);
 
     const data = await res.json();
     return data.diploma;
   } catch (error) {
     console.error('Error fetching diploma:', error);
-    throw error; // Propaga el error para que pueda ser manejado en el código que llama a esta función
+    throw error;
   }
 };
